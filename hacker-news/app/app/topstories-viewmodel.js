c80c0fdadf2c35fe501cccc309d1b0f579004390
qpautrat <quentin.pautrat@gmail.com>
2015-04-16 19:33:37 +0200

    Create a view model

---
+++ hacker-news/app/app/topstories-viewmodel.js
@@ -0,0 +1,35 @@
+viewModel = {};
+
+viewModel.topstories = [];
+
+viewModel.loadStories = function() {
+    this.topstories = [
+        {
+            'title': 'Nokia Agrees to Buy Alcatel-Lucent for $16.6B',
+            'by': 'alphadevx',
+            'score': 39
+        },
+        {
+            'title': 'Boolean parameters to API functions considered harmful (2011)',
+            'by': 'luu',
+            'score': 69
+        },
+        {
+            'title': '32-bit X86 Position Independent Code – It\'s That Bad',
+            'by': 'cremno',
+            'score': 63
+        },
+        {
+            'title': 'Finding bugs in SQLite, the easy way',
+            'by': 'robin_reala',
+            'score': 275
+        },
+        {
+            'title': 'New Mexico outlaws civil asset forfeiture',
+            'by': 'hackercurious',
+            'score': 78
+        }
+    ];
+}
+
+module.exports = viewModel;
--- hacker-news/app/app/topstories.js
+++ hacker-news/app/app/topstories.js
@@ -1,34 +1,7 @@
-var model = {
-    'topstories' : [
-        {
-            'title': 'Nokia Agrees to Buy Alcatel-Lucent for $16.6B',
-            'by': 'alphadevx',
-            'score': 39
-        },
-        {
-            'title': 'Boolean parameters to API functions considered harmful (2011)',
-            'by': 'luu',
-            'score': 69
-        },
-        {
-            'title': '32-bit X86 Position Independent Code – It\'s That Bad',
-            'by': 'cremno',
-            'score': 63
-        },
-        {
-            'title': 'Finding bugs in SQLite, the easy way',
-            'by': 'robin_reala',
-            'score': 275
-        },
-        {
-            'title': 'New Mexico outlaws civil asset forfeiture',
-            'by': 'hackercurious',
-            'score': 78
-        }
-    ]
-};
+var model = require("./topstories-viewmodel");
 
 exports.pageLoaded = function(args) {
     var page = args.object;
+    model.loadStories();
     page.bindingContext = model;
 }
